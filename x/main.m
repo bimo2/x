ea421217c660b11ad4f34a0204586a530fcab8cb
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2023-12-21 08:12:28 -0500

    setup xcode 15 cli app

---
+++ x/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  x
+//
+//  Created by Bimal Bhagrath on 2023-12-21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

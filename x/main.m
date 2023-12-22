17bd52a45326f75a76e1fd9312930c805ec798e6
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2023-12-22 12:39:52 -0500

    setup buildkite ci pipeline

--- x/main.m
+++ x/main.m
@@ -7,10 +7,15 @@
 
 #import <Foundation/Foundation.h>
 
+#ifndef BUILD_NUMBER
+#define BUILD_NUMBER 1
+#endif
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"x build#%d", BUILD_NUMBER);
     }
+
     return 0;
 }
